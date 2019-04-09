50d41bb5e829c09664271da440f66fe9a05e80e8
王元炜 <davidwangnoi@163.com>
2019-04-09 16:47:38 +0800

    完成点的标注和对应

--- app/Main.hs
+++ app/Main.hs
@@ -7,10 +7,10 @@ import Text.Printf
 
 printFunc :: Context -> Context -> Int -> IO ()
 printFunc source target t = putStrLn (printf "solving %d..." t) >>
-    savePngImage (printf "sources/result1/%d.png" t) (ImageRGBF (getImage $ buildMorphing (realToFrac t / 64.0) source target))
+    savePngImage (printf "sources/result2/%d.png" t) (ImageRGBF (getImage $ buildMorphing (realToFrac t / 64.0) source target))
 
 main = do 
     putStrLn "Compiled Success!, 请输入位置信息名称"
-    source <- readContext "sources/source1"
-    target <- readContext "sources/target1"
+    source <- readContext "sources/target2"
+    target <- readContext "sources/source2"
     mapM_ (printFunc source target) [0..64]
